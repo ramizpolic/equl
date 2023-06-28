2683a0166ea24b5d0369ee8408aa6b63949cb97f
Ramiz Polic <ramiz.polic@hotmail.com>
2023-06-28 14:51:34 +0200

    add recursive map extractor

--- compressed_map.go
+++ compressed_map.go
@@ -1,10 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // CompressedMap defines a map with keys such as: key.a.b, or key.[0]b.c
-type CompressedMap struct {
-	v map[string]interface{}
+type extracted struct {
+	req   []string
+	orig  map[string]interface{}
+	fresh map[string]interface{}
 }
 
-func (m *CompressedMap) Get(key string) (interface{}, error) {
-	return m.v[key], nil
+func unwrapMap(data map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for k, v := range data {
+		switch v.(type) {
+		case map[string]interface{}:
+			for ck, cv := range unwrapMap(v.(map[string]interface{})) {
+				result[fmt.Sprintf("%s.%s", k, ck)] = cv
+			}
+		default:
+			result[k] = v
+		}
+	}
+	return result
+}
+
+func newExtractedWith(orig map[string]interface{}, req []string) *extracted {
+	r := &extracted{
+		req:   req,
+		orig:  orig,
+		fresh: make(map[string]interface{}),
+	}
+	return r
+}
+
+func matches(key string, reqs []string) bool {
+	for _, req := range reqs {
+		if yes, _ := regexp.Match(req, []byte(key)); yes {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/ramizpolic/map-differ/diff"
 )
 
+type Object struct {
+	A string
+	B []string
+	C struct {
+		D string
+		F float64
+		M struct {
+			K map[string]int
+			G int
+			H struct {
+				A string
+			}
+		}
+	}
+}
+
+var obj = Object{
+	A: "a",
+	B: []string{"b", "c"},
+	C: struct {
+		D string
+		F float64
+		M struct {
+			K map[string]int
+			G int
+			H struct {
+				A string
+			}
+		}
+	}{
+		D: "D",
+		F: 1.26,
+		M: struct {
+			K map[string]int
+			G int
+			H struct{ A string }
+		}{K: map[string]int{"a": 1, "b": 2}, G: 1, H: struct{ A string }{A: "child"}},
+	},
+}
+
 func main() {
-	fmt.Println(diff.Exported)
+	v, _ := json.Marshal(obj)
+	var data map[string]interface{}
+	_ = json.Unmarshal(v, &data)
+	fmt.Println(unwrapMap(data))
 }
